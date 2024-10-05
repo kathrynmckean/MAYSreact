72fe8f98431e8d1bbfb119f678d8b675bbabcaa2
Kathryn <kathryn@Kathryns-MacBook-Pro.local>
2024-10-05 11:41:27 -0500

    fleshing out the header and navigation

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,55 +3,20 @@ import logo from './logo.svg';
 import { Grid } from '@mui/material';
 function Header () {
     return (
+
         <header>
-            {/* <img src={logo} className="logo" alt="logo" />
-       
+            {/* <div className="logoContainer"> */}
+               <img src={logo} className="logo" alt="logo" /> 
+            {/* </div> */}
+
+            <div className="navContainer">
                 <div className="phone">(214)123-2345</div>
-                <div className='nav'>
-                <button>Services</button>
-                <button>Booking</button>
-                <button>About Us</button>
-                    </div> */
- 
-
-/* <img src={logo} className="logo" alt="logo" />
-<nav>
-    <button>Services</button>
-    <button>Booking</button>
-    <button>About</button>
-</nav> */
-
-                    }
-                   <Grid container spacing={2}>
-    <Grid item xs={4}>
-      {/* <p>blank</p> */}
-    </Grid>
-    <Grid item xs={4}>
-    <img src={logo} className="logo" alt="logo" />
-    </Grid>
-    <Grid item xs={4}>
-        <Grid className='nav' container spacing = {2}>
-            <Grid item xs={12}>
-                <button>(214)123-2345</button>
-            </Grid>
-            <Grid item xs={12}>
-                <Grid container spacing = {2}>
-                    <Grid item xs={4}>
-                        <button>Services</button>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <button>Booking</button>
-                    </Grid>
-                    <Grid item xs={4}>
-                        <button>About</button>
-                    </Grid>
-                </Grid>
-            </Grid>
-        </Grid>
-    </Grid>
-</Grid>
-
-
+                <nav >
+                    <button className="navButton">Services</button>
+                    <button className="navButton">Booking</button>
+                    <button className="navButton">About Us</button>
+                </nav>
+            </div>
         </header>
 
     )
